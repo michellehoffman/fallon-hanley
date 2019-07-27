a5681f5de2703e16fe446a595a28eca351fbb804
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2019-07-27 08:36:40 -0600

    Resolve error with binding to port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-var path = require('path');
 
 app.use(express.static('public'));
-app.listen(3000);
+
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'));
