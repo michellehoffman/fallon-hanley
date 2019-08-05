03cfd05056647a7c4761bdbf2cef9bccbf28ddfc
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2019-08-05 08:03:24 -0600

    Add focus styling to label when input focused

--- public/scripts.js
+++ public/scripts.js
@@ -1,4 +1,8 @@
 $('nav li').on('click', scroll);
+$('.contact input').on('focus', focusLabel);
+$('.contact input').on('blur', unfocusLabel);
+$('.contact textarea').on('focus', focusLabel);
+$('.contact textarea').on('blur', unfocusLabel);
 
 function scroll() {
   var section = this.id;
@@ -8,5 +12,15 @@ function scroll() {
 function scrollTo(name) {
   $('html, body').animate({
     scrollTop: ($(`section.${ name }`).offset().top)
-  })
+  });
+}
+
+function focusLabel() {
+  var label = $(this).siblings('label');
+  $(label).addClass('focus');
+}
+
+function unfocusLabel() {
+  var label = $(this).siblings('label');
+  $(label).removeClass('focus');
 }
