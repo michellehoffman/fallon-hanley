264fd4fc50650a7bea290471bd148bcfefb7b62f
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2019-08-09 08:05:06 -0600

    Validate email on blur

--- public/scripts.js
+++ public/scripts.js
@@ -3,6 +3,7 @@ $('.contact input').on('focus', focusLabel);
 $('.contact input').on('blur', unfocusLabel);
 $('.contact textarea').on('focus', focusLabel);
 $('.contact textarea').on('blur', unfocusLabel);
+$('#contact-email').on('blur', validateEmail);
 
 function scroll() {
   var section = this.id;
@@ -24,3 +25,29 @@ function unfocusLabel() {
   var label = $(this).siblings('label');
   $(label).removeClass('focus');
 }
+
+function displayError(element, message) {
+  $(element).text(message);
+}
+
+function removeError(element) {
+  $(element).text('');
+}
+
+function testEmail(email) {
+  var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(email);
+}
+
+function validateEmail(e) {
+  var email = String(this.value).toLowerCase();
+  var validEmail = testEmail(email);
+  var errorContainer = $(this).siblings('.error-container');
+  var invalidMessage = 'Email must be a valid format';
+
+  validEmail ? removeError(errorContainer) : displayError(errorContainer, invalidMessage);
+}
+
+
+
+
